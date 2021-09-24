a665ac195fe14313476c34ef6397dfe49f39649d
Knot <knotto.vt@gmail.com>
2021-09-24 15:01:46 +0700

    1.3.1

--- AppStoreManager.podspec
+++ AppStoreManager.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |spec|
 
   spec.name         = "AppStoreManager"
-  spec.version      = "1.3.0"
+  spec.version      = "1.3.1"
   spec.summary      = "A new version checking framework in Swift."
 
   spec.homepage     = "https://knottx.github.io"
--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "AppStoreManager",
     platforms: [
-        .iOS(.v8)
+        .iOS(.v10)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
